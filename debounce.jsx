fde6b098c29a23adfe35975c51400e28a247034c
shufufufu <rem2248668357@163.com>
2025-03-28 19:49:00 +0800

    debounce change

--- debounce.jsx
+++ debounce.jsx
@@ -1,28 +1,28 @@
-function debounce(func,wait,immediate){
-    let timeout,result
-    const debounced = function(){
-        const context = this
-        const args = arguments
+function debounce(func, wait, immediate) {
+  let timeout, result;
+  const debounced = function () {
+    let context = this;
+    let args = arguments;
 
-        if(timeout) clearTimeout(timeout)
-        if(immediate){
-            const callnow = !timeout
-            timeout = setTimeout(function(){
-                timeout = null
-            },wait)
-            if(callnow) result = func.apply(context,args)
-        }else{
-            timeout = setTimeout(function(){
-                result = func.apply(context,args)
-            },wait)
-        }
-        return result  
+    if (timeout) clearTimeout(timeout);
+    if (immediate) {
+      const callnow = !timeout;
+      timeout = setTimeout(function () {
+        timeout = null;
+      }, wait);
+      if (callnow) result = func.apply(context, args);
+    } else {
+      timeout = setTimeout(function () {
+        result = func.apply(context, args);
+      }, wait);
     }
+    return result;
+  };
 
-    debounced.cancel = function () {
-        clearTimeout(timeout)
-        timeout = null
-      }
-    
-      return debounced
+  debounced.cancel = function () {
+    clearTimeout(timeout);
+    timeout = null;
+  };
+
+  return debounced;
 }
